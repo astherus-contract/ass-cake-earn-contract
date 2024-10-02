15c1dfed92effbe6a2a90069f251b88a8bf54ff2
Lawson Cheng <lawson.c@binance.com>
2024-10-02 16:18:32 +0800

    Added UniversalProxy contract

--- src/interfaces/IUniversalProxy.sol
+++ src/interfaces/IUniversalProxy.sol
@@ -3,5 +3,7 @@ pragma solidity ^0.8.20;
 
 /// @title Universal Proxy Contract interface
 /// @notice Using for other protocols proxy call, for example: PancakeSwap, StakeDAO and etc.
-interface IRewardDistributionScheduler {
+interface IUniversalProxy {
+  function increaseLock(uint256 amount) external;
+  function extendLock(uint256 unlockTime) external;
 }
